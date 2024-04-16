cf5fa7f7102034b9b6aeb6120c2976fd1d04cb0c
CJM <okck12382@gmail.com>
2024-04-16 13:20:28 +0900

    헤더 이름 추가

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const bg = document.querySelector('.background');
+const btnList = document.querySelector('.btnbox');
 const prev = document.querySelector('.prev');
 const next = document.querySelector('.next');
 let vw = 0;
+let index = 1;
 
-prev.addEventListener('click',function () {
-    vw === 0 ? vw -= 500 : vw += 100;
-
+prev.addEventListener('click', function () {
+    if (vw === 0) {
+        vw -= 500;
+        index = 6;
+    } else {
+        index--;
+        vw += 100;
+    }
+    [...btnList.children].forEach(e=>e.style.background = 'white');
+    btnList.children[index].style.background = '#eee';
+    
     bg.style.transform = `translateX(${vw}vw)`;
 })
-next.addEventListener('click',function () {
-    vw === -500 ? vw = 0 : vw -= 100;
+next.addEventListener('click', function () {
+    if(vw === -500){
+        index = 1;
+        vw = 0;
+    }else{
+        index++;
+        vw -= 100;
+    }
+    [...btnList.children].forEach(e=>e.style.background = 'white');
+    btnList.children[index].style.background = '#eee';
     bg.style.transform = `translateX(${vw}vw)`;
 })
+
