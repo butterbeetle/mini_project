d7e32fe26c2a702a75effad3fb3b96d032f68b6c
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-04-17 12:17:47 +0900

    일부 수정

---
+++ detail.js
@@ -0,0 +1,7 @@
+const prevBtn = document.querySelector(".prev");
+
+function clickPrevBtn(e){
+    window.location.href = "https://www.pinterest.co.kr/";
+}
+
+prevBtn.addEventListener("click", clickPrevBtn);
