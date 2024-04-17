82ca2a0358d23925ec28939eddf5899ab002eb3e
yeliinbb <156063434+yeliinbb@users.noreply.github.com>
2024-04-17 15:01:55 +0900

    캐러셀 시도

--- detail.js
+++ detail.js
@@ -1,7 +1,30 @@
 const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const carouselSlide = document.querySelector(".carousel-slide");
+const carousel = document.querySelectorAll(".carousel");
+
+let index = 0;
+const itemNumbers = carousel.length;
+// 캐러셀 아이템 갯수 지정
+console.log(itemNumbers);
 
 function clickPrevBtn(e){
-    window.location.href = "https://www.pinterest.co.kr/";
+    if (index === 0) {
+        return;
+    } else if (index -= 1) {
+        carouselSlide.style.transform = `translateX(-100vw)`;
+
+    }
+}
+
+function clickNextBtn() {
+    if (index === itemNumbers - 1) {
+        return;
+    } else if (index += 1) {
+        carouselSlide.style.transform = `translateX(100vw)`;
+    }
+
 }
 
 prevBtn.addEventListener("click", clickPrevBtn);
+nextBtn.addEventListener("click", clickNextBtn);
